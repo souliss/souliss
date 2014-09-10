a31dbc23610eede751b9bda4f06b758ae141771a
dario <dario.dimaio@souliss.net>
2014-09-11 01:00:05 +0200

    If the bus is busy wait and retry

--- Souliss/frame/vNet/drivers/usart/vNetDriver_usart.cpp
+++ Souliss/frame/vNet/drivers/usart/vNetDriver_usart.cpp
@@ -30,7 +30,7 @@
 #include "vNetDriver_usart.h"							
 
 // Global variables used in the driver
-uint8_t usartframe[USART_FRAME_LEN], l=0;	
+uint8_t usartframe[USART_FRAME_LEN], l=0, retry=USART_MAXRETRY;	
 uint8_t	busstate=USART_BUSBUSY;
 uint16_t myaddress=0, caindex=0, in_crc=0;
 
@@ -133,50 +133,66 @@ uint8_t vNet_Send_M5(uint16_t addr, oFrame *frame, uint8_t len)
 		USART_LOG("(USART)<Send> Try\r\n");
 		#endif
 	
-		// The bus was used recently, if not yet in use try to get it
-		if(!USARTDRIVER.available())
-		{	
-			// Set the write mode pin of the RS485
-			#if(USART_TXENABLE)
-			digitalWrite(USART_TXENPIN, HIGH);
-			#endif
-	
-			// Send a token to notify that we are willing to use the bus
-			for(i=0; i<USART_TOKEN_LENGHT; i++)
-				USARTDRIVER.write(USART_TOKEN);	
-			USARTDRIVER.flush();					// Wait data send
-			
-			// Set the write mode pin of the RS485
-			#if(USART_TXENABLE)
-			digitalWrite(USART_TXENPIN, LOW);
-			#endif
+		while(retry)
+		{
+			// The bus was used recently, if not yet in use try to get it
+			if(!USARTDRIVER.available())
+			{	
+				// Set the write mode pin of the RS485
+				#if(USART_TXENABLE)
+				digitalWrite(USART_TXENPIN, HIGH);
+				#endif
+		
+				// Send a token to notify that we are willing to use the bus
+				for(i=0; i<USART_TOKEN_LENGHT; i++)
+					USARTDRIVER.write(USART_TOKEN);	
+				USARTDRIVER.flush();					// Wait data send
 				
-			// Wait for a given number of token times before proceed,
-			// nodes with lower address has major priority.
-			// This is a collision avoidance with fixed priority, in
-			// a busy network, nodes with lower priority may never 
-			// found a slot
-			waitBusFree();
+				// Set the write mode pin of the RS485
+				#if(USART_TXENABLE)
+				digitalWrite(USART_TXENPIN, LOW);
+				#endif
+					
+				// Wait for a given number of token times before proceed,
+				// nodes with lower address has major priority.
+				// This is a collision avoidance with fixed priority, in
+				// a busy network, nodes with lower priority may never 
+				// found a slot
+				waitBusFree();
+				
+				// Check if someone started while waiting for the slot
+				if(USARTDRIVER.available())
+				{
+					// From the USART we get one byte per time, so before has to be identified
+					// if there is an incoming frame	
+					while(USARTDRIVER.available() && (l < USART_FRAME_LEN))	
+						usartframe[l++] = USARTDRIVER.read();
+
+					retry--;	// Retry
+				}	
+			}
+			else
+			{
+				// From the USART we get one byte per time, so before has to be identified
+				// if there is an incoming frame	
+				while(USARTDRIVER.available() && (l < USART_FRAME_LEN))	
+					usartframe[l++] = USARTDRIVER.read();
+
+				retry--;		// Retry
+			}
 			
-			// Check if someone started while waiting for the slot
-			if(USARTDRIVER.available())
+			// No more retries available, return
+			if(retry==0)
 			{
 				#if(USART_DEBUG)	
 				USART_LOG("(USART)<Send> Bus busy\r\n");
 				#endif
-			
-				BusIsBusy();
+					
 				return USART_FAIL;
-			}	
+			}
+			else
+				waitSend();		// Wait a time of a frame
 		}
-		else
-		{
-			#if(USART_DEBUG)	
-			USART_LOG("(USART)<Send> Waiting data\r\n");
-			#endif
-			
-			return USART_FAIL;
-		}	
 	}
 	#endif
 	
@@ -184,6 +200,9 @@ uint8_t vNet_Send_M5(uint16_t addr, oFrame *frame, uint8_t len)
 	USART_LOG("(USART)<Send> Bus free\r\n");
 	#endif	
 
+	// Reset the number of available retry
+	retry = USART_MAXRETRY;
+	
 	// Set the write mode pin of the RS485
 	#if(USART_TXENABLE)
 	digitalWrite(USART_TXENPIN, HIGH);
@@ -515,7 +534,7 @@ uint8_t vNet_RetrieveData_M5(uint8_t *data)
 		// Move forward not parsed data
 		memmove(usartframe, usartframe+len, USART_FRAME_LEN-len);
 		if(l>(USART_FRAME_LEN-len))
-			l-=USART_FRAME_LEN-len;				// Reset the lenght
+			l-=USART_FRAME_LEN-len;				// Reset the length
 		else
 			l = 0;
 	}
