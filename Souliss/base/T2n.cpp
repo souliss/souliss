c86340492f32f4ab8e71471ed7b2690573dda745
FulvioSpelta <fulvio.spelta@gmail.com>
2014-11-06 01:38:16 +0100

    Comments updated, debug instructions removed

--- Souliss/base/T2n.cpp
+++ Souliss/base/T2n.cpp
@@ -181,14 +181,28 @@ void Souliss_SetT22(U8 *memory_map, U8 slot)
 			Using a monostable wall switch (press and spring return) or a 
 			software command from user interface, each press will toogle 
 			the output status.			
+				#define Souliss_T2n_CloseCmd_Local	0x05
+				#define Souliss_T2n_OpenCmd_Local		0x06
+				#define Souliss_T2n_StopCmd					0x04
+				
+			Following constant are defined for sketch source compatibility with versions < A6.1.1
+			and their use is now deprecated.
 				#define Souliss_T2n_CloseCmd		0x01
 				#define Souliss_T2n_OpenCmd			0x02
-				#define Souliss_T2n_StopCmd			0x03
+
+			This commands are designed to be used by an application (Souliss App, OpenHAB binding, user applications)
+			in order to support software "scenario". When the Open/Close command is recevived it is always excuted;
+			if the motor is running opposite direction it stops for 4 cycles then it revert motion.
+				#define Souliss_T2n_CloseCmd_SW			0x01
+				#define Souliss_T2n_OpenCmd_SW			0x02
+				#define Souliss_T2n_StopCmd					0x03
 				
 		Command recap, using: 
-		-  1(hex) as command, CLOSE request (stop if opening) 
-		-  2(hex) as command, OPEN request (stop if closing)
-		-  3(hex) as command, STOP request
+		-  1(hex) as command, Software CLOSE request (stop 4 cycles if opening) 
+		-  2(hex) as command, Software OPEN request (stop 4 cycles if closing)
+		-  4(hex) as command, STOP request
+		-  5(hex) as command, CLOSE request (stop if opening) 
+		-  6(hex) as command, OPEN request (stop if closing)
 		
 		Output status:
 		- 1(hex) for CLOSE,
@@ -248,10 +262,6 @@ U8 Souliss_Logic_T22(U8 *memory_map, U8 slot, U8 *trigger)
 		// If a command was issued, set the timer
 		if(!Souliss_T2n_IsTemporaryStop)
 		{
-			Serial.print("Set timer: 0x");
-			Serial.print(Souliss_T2n_Timer_Val, HEX);
-			Serial.print("\n\r");
-			
 			memory_map[MaCaco_AUXIN_s + slot] = Souliss_T2n_Timer_Val;		// Set timer value
 			memory_map[MaCaco_IN_s + slot] = Souliss_T2n_RstCmd;					// Reset command
 			
@@ -297,20 +307,11 @@ U8 Souliss_Logic_T22(U8 *memory_map, U8 slot, U8 *trigger)
 /**************************************************************************/
 void Souliss_T22_Timer(U8 *memory_map, U8 slot)
 {
-			Serial.print("STATO: 0x");
-			Serial.print(memory_map[MaCaco_OUT_s + slot], HEX);
-			Serial.print("\n\r");
-
 			// Memory value is used as timer
 	if(((memory_map[MaCaco_AUXIN_s + slot] > Souliss_T2n_Timer_Off) &&
-		(memory_map[MaCaco_AUXIN_s + slot] <= Souliss_T2n_Timer_Val)) ||
-		
+		(memory_map[MaCaco_AUXIN_s + slot] <= Souliss_T2n_Timer_Val)) || 
 		Souliss_T2n_IsTemporaryStop )
 	{	
-			Serial.print("Timer: 0x");
-			Serial.print(memory_map[MaCaco_AUXIN_s + slot], HEX);
-			Serial.print("\n\r");
-
 			memory_map[MaCaco_AUXIN_s + slot]--;									// Decrease timer
 	}	
 }
