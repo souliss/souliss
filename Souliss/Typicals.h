5339ffe75b96f12259c696b0f23ae406165854bf
dario <dario.dimaio@souliss.net>
2014-11-07 13:04:32 +0100

    Public release for Souliss Alpha 6.1.2

--- Souliss/Typicals.h
+++ Souliss/Typicals.h
@@ -126,13 +126,13 @@
 #define Souliss_T1n_RGBLamp_B5			0xD4
 
 // General defines for T2n
-#define Souliss_T2n_CloseCmd_SW		0x01		// Close Command 
-#define Souliss_T2n_OpenCmd_SW		0x02		// Open Command
-#define Souliss_T2n_CloseCmd			Souliss_T2n_CloseCmd_SW		// Close Command for previous versione compatibility
-#define Souliss_T2n_OpenCmd				Souliss_T2n_OpenCmd_SW		// Open Command for previous versione compatibility
+#define Souliss_T2n_CloseCmd_SW			0x01		// Close Command 
+#define Souliss_T2n_OpenCmd_SW			0x02		// Open Command
+#define Souliss_T2n_CloseCmd			Souliss_T2n_CloseCmd_SW		// Close Command (legacy)
+#define Souliss_T2n_OpenCmd				Souliss_T2n_OpenCmd_SW		// Open Command (legacy)
 #define Souliss_T2n_StopCmd				0x04		// Stop Command
-#define Souliss_T2n_CloseCmd_Local			0x08		// Close Command (only from local pushbutton)
-#define Souliss_T2n_OpenCmd_Local				0x10		// Open Command (only from local pushbutton)
+#define Souliss_T2n_CloseCmd_Local		0x08		// Close Command (only from local pushbutton)
+#define Souliss_T2n_OpenCmd_Local		0x10		// Open Command (only from local pushbutton)
 #define Souliss_T2n_ToogleCmd			0x08		// Toogle Command
 #define Souliss_T2n_RstCmd				0x00		// Reset Command
 #define Souliss_T2n_Timer_Val			0xAF		// Timer set value
