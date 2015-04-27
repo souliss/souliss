77fb0663441604659af1da977c1b11c066af17cc
juanpintom <juanpintom@gmail.com>
2015-04-27 23:07:53 +0200

    Bug on line 36 repaired

--- frame/vNet/drivers/ethENC28J60/vNetDriver_brd.h
+++ frame/vNet/drivers/ethENC28J60/vNetDriver_brd.h
@@ -33,7 +33,7 @@
 #include "vNetDriver_eth.h"
 
 #define	vNet_Init_M3()						vNet_Init_M1()
-#dedine vNet_Begin_M3						vNet_Begin_M1
+#define vNet_Begin_M3						vNet_Begin_M1
 #define	vNet_DataAvailable_M3()				vNet_DataAvailable_M1()
 #define	vNet_RetrieveData_M3(data)			vNet_RetrieveData_M1(data)
 
