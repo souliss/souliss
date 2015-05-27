00a474a5872fadba3f03002e07effe84621d605f
juanpintom <juanpintom@gmail.com>
2015-05-27 16:23:47 +0200

    Create E02_ESP_Dallas.ino

---
+++ E02_ESP_Dallas.ino
@@ -0,0 +1,61 @@
+// Configure the framework
+#include "bconf/MCU_ESP8266.h"              // Load the code directly on the ESP8266
+#include "conf/Gateway.h"                   // The main node is the Gateway, we have just one node
+#include "conf/DynamicAddressing.h"         // Use dynamic addressing
+
+// Define the WiFi name and password
+#define WIFICONF_INSKETCH
+#define WiFi_SSID               "SSID"
+#define WiFi_Password           "PASSWORD"    
+
+// Include framework code and libraries
+#include <ESP8266WiFi.h>
+#include <EEPROM.h>
+#include "Souliss.h"
+
+// This identify the number of the SLOT logic
+#define DALLAS              0
+
+#define DALLASPIN 4 //Se declara el pin donde se conectará la DATA
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+OneWire ourWire(DALLASPIN); //Se establece el pin declarado como bus para la comunicación OneWire
+DallasTemperature sensors(&ourWire); //Se instancia la librería DallasTemperature
+
+#define Debug Serial        //Change to Serial1 if you want to use the GPIO2 to TX
+
+void setup()
+{   
+    Initialize();
+    
+    Serial.begin(115200);
+
+    sensors.begin(); //Se inician los sensores DS18B20
+    // Connect to the WiFi network and get an address from DHCP
+    GetIPAddress();      
+    SetAsGateway(myvNet_dhcp); 
+ 
+    Set_Temperature(DALLAS);
+}
+
+void loop()
+{ 
+    EXECUTEFAST() {                     
+        UPDATEFAST();   
+
+        FAST_910ms()    {
+          // Acquire temperature from the microcontroller ADC
+          sensors.requestTemperatures(); //Prepara el sensor para la lectura
+          float dallas = sensors.getTempCByIndex(0);
+          Souliss_ImportAnalog(memory_map, DALLAS, &dallas);
+        }      
+
+        FAST_2110ms()
+        {
+           Logic_Temperature(DALLAS);
+        } 
+         // Here we handle here the communication with Android
+        FAST_GatewayComms();                                        
+    }
+} 
