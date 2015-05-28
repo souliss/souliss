64b9956e4aaf5603f5ba6dc4b2be44b6acff7ae1
dario <dario.dimaio@souliss.net>
2015-05-29 00:30:56 +0200

    Added ESP8266 USART / RS485 example

---
+++ examples/WiFi/e01_WiFitoRS485_n1/e01_WiFitoRS485_n1.ino
@@ -0,0 +1,67 @@
+/**************************************************************************
+    Souliss - Bridge WiFi to RS485
+    
+    This is the basic example, create a software push-button on Android
+    using SoulissApp (get it from Play Store) and bridge to an RS485 peer. 
+    
+    Load this code on ESP8266 board using the porting of the Arduino core
+    for this platform.
+        
+***************************************************************************/
+
+// Configure the framework
+#include "bconf/MCU_ESP8266.h"              // Load the code directly on the ESP8266
+#include "conf/Gateway.h"                   // The main node is the Gateway, we have just one node
+#include "con/usart.h"                      // USART / RS485 transceiver
+#include "conf/IPBroadcast.h"
+
+// **** Define the WiFi name and password ****
+#define WIFICONF_INSKETCH
+#define WiFi_SSID               "mywifi"
+#define WiFi_Password           "mypassword"    
+
+// Include framework code and libraries
+#include <ESP8266WiFi.h>
+#include <EEPROM.h>
+#include "Souliss.h"
+
+// This identify the number of the LED logic
+#define MYLEDLOGIC          0               
+
+// **** Define here the right pin for your ESP module **** 
+#define OUTPUTPIN           5
+
+void setup()
+{   
+    Initialize();
+
+    // Connect to the WiFi network and get an address from DHCP
+    GetIPAddress();                           
+    SetAsGateway(myvNet_dhcp);       // Set this node as gateway for SoulissApp  
+
+    // This is the vNet address for this node, used to communicate with other
+    // nodes in your Souliss network
+    SetAddress(0xAB01, 0xFF00, 0x0000);
+    SetAddress(0xCE01, 0xFF00, 0x0000)
+    SetAsPeerNode(0xCE02, 1);
+    
+    Set_SimpleLight(MYLEDLOGIC);        // Define a simple LED light logic
+    
+    pinMode(OUTPUTPIN, OUTPUT);         // Use pin as output 
+}
+
+void loop()
+{ 
+    // Here we start to play
+    EXECUTEFAST() {                     
+        UPDATEFAST();   
+        
+        FAST_50ms() {   // We process the logic and relevant input and output every 50 milliseconds
+            Logic_SimpleLight(MYLEDLOGIC);
+            DigOut(OUTPUTPIN, Souliss_T1n_Coil,MYLEDLOGIC);
+        } 
+              
+        // Here we handle here the communication with Android
+        FAST_GatewayComms();                                        
+    }
+} 
