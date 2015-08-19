1e5d042b09a4339f8aae2190c86cf86cbc4e7908
juanpintom <juanpintom@gmail.com>
2015-08-19 16:18:30 +0200

    Create Page_Admin.h
    Copied from https://github.com/juanpintom/Souliss_ESP_Examples/blob/master/SoulissDomo_Device_Example/Souliss_ESPv3_5/Page_Admin.h

---
+++ SoulissDomo_Device_Example/Souliss_ESP_WebInterface_Simplified/Page_Admin.h
@@ -0,0 +1,38 @@
+//
+//  HTML PAGE
+//
+
+const char PAGE_AdminMainPage[] PROGMEM = R"=====(
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<h2>Souliss Node Administration </h2>
+<hr>
+<a href="general.html" style="width:250px" class="btn btn--m btn--blue" >General Configuration</a><br>
+<a href="config.html" style="width:250px" class="btn btn--m btn--blue" >Network Configuration</a><br>
+<a href="info.html"   style="width:250px"  class="btn btn--m btn--blue" >Network Information</a><br>
+<a href="ntp.html"   style="width:250px"  class="btn btn--m btn--blue" >NTP Settings</a><br>
+<a href="main.html"   style="width:250px"  class="btn btn--m btn--blue" >Main Interface</a><br>
+
+<hr>
+<strong>Clicking Main Interface Button will leaving Access Point Mode, </strong>
+<strong>Please make a note of your IP Address Node in Network Information </strong>
+<p><strong>You can enter this Web Interface again by your NEW IP Address Node given from Network Information. </strong></p>
+
+<script>
+window.onload = function ()
+{
+	load("style.css","css", function() 
+	{
+		load("microajax.js","js", function() 
+		{
+		
+		
+
+		});
+	});
+}
+function load(e,t,n){if("js"==t){var a=document.createElement("script");a.src=e,a.type="text/javascript",a.async=!1,a.onload=function(){n()},document.getElementsByTagName("head")[0].appendChild(a)}else if("css"==t){var a=document.createElement("link");a.href=e,a.rel="stylesheet",a.type="text/css",a.async=!1,a.onload=function(){n()},document.getElementsByTagName("head")[0].appendChild(a)}}
+
+</script>
+
+)=====";
