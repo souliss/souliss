6cfab7f0c8b0b1b52395ed7759fa5e1d37e6af1e
dario <dario.dimaio@souliss.net>
2015-09-05 21:09:28 +0200

    Updated Living Room examples

--- examples/plinio/e01_LivingRoom/e01_LivingRoom.ino
+++ examples/plinio/e01_LivingRoom/e01_LivingRoom.ino
@@ -10,26 +10,31 @@
 	<<<<<<<<
 	
 ***************************************************************************/
-#include "bconf/StandardArduino.h"				// Use an Arduino board
-#include "bconf/IoTuino_RadioOnly_W5100.h"		// Define the board type
-#include "conf/ethW5100.h"						// Ethernet through Wiznet W5100
-#include "conf/Gateway.h"						// Define a gateway
+#include "bconf/StandardArduino.h"              // Use an Arduino board
+#include "bconf/LYT88_LEDBulb_Radio.h"          // Define the board type
+#include "conf/usart_fast.h"                    // Ethernet through Wiznet W5100
 
 // Include framework code and libraries
 #include <SPI.h>
 #include "Souliss.h"
+
+// Include Authometion libraries
+#include <PL1167.h>
+#include <Lytwifi.h>
+#include <WiFiInterrupt.h>
 #include "DHT.h"
 
-// Define logic slots, multicolor lights use four slots
-#define LYTLIGHT1			0					
-#define	LIGHTINTESITY		4
-#define	TEMPERATURE			6
-#define	HUMIDITY			8
+// There are not used in this sketch, keep it just to pat the compiler
+#include <EEPROM.h>
+#include <SoftwareSerial.h>
 
-// Define the network configuration
-uint8_t ip_address[4]  = {192, 168, 3, 88};
-uint8_t subnet_mask[4] = {255, 255, 255, 0};
-uint8_t ip_gateway[4]  = {192, 168, 3, 1};
+// Define logic slots, multicolor lights use four slots
+#define LYTLIGHT1			0	
+#define LYTLIGHT2			4	
+#define LYTLIGHT3			8	
+#define	LIGHTINTESITY		9
+#define	TEMPERATURE			11
+#define	HUMIDITY			13
 
 uint8_t toggleswitch = 0;
 
@@ -38,30 +43,32 @@ float th=0;
 
 void setup()
 {	
+    Initialize();	
+
 	// Set the inputs
 	pinMode(8,INPUT);	
 	pinMode(A0, INPUT);
 	dht.begin();
-	
-	// Init the IoTuino radio using pin9 as chip select for the SPI communication
-	InitIoTuino_RadioOnly(9);
 
-	// Setup the IP configuration
-	//
-	// The vNet address will be equal to the last byte of the IP address
-	// with the most significant one at zero, so in this case 0x0011.
-	// Board reply to ping at 192.168.1.17
-	Souliss_SetIPAddress(ip_address, subnet_mask, ip_gateway);
-	SetAsGateway((U16)ip_address[3]);
+    // Set network parameters
+    SetAddress(0xCE02, 0xFF00, 0xCE01);
 	
-	// Se the LYT bulbs (index, bulb type, addr_a, addr_b, logic slot)
-	SetLYT(0, 2, 0, 1, LYTLIGHT1);
+    // Se the LYT bulbs (index, bulb type, addr_a, addr_b, logic slot), here we have
+    // the first lamp (index 0) with default addresses (0, 0). Change these based on
+    // the configuration of your lamps.
+    InitLYT();
+    SetLYT(0, 0, 1, LYTLIGHT1);
+    SetLYT(1, 0, 2, LYTLIGHT2);
+    SetLYT(2, 0, 3, LYTLIGHT3);
+
+    // Define a logic to handle the bulb(s)
+    SetLYTLamps(LYTLIGHT1); 
+    SetLYTLamps(LYTLIGHT2); 
+    SetLYTLamps(LYTLIGHT3); 
+
 	Set_Light(LIGHTINTESITY);
 	Set_Temperature(TEMPERATURE);
 	Set_Humidity(HUMIDITY);
-	
-	// Define a logic to handle the bulb(s)
-	SetLYTLamps(LYTLIGHT1);	
 }
 
 void loop()
@@ -70,61 +77,67 @@ void loop()
 	EXECUTEFAST() {						
 		UPDATEFAST();	
 		
-		FAST_10ms() {	// We process the logic and relevant input and output every 110 milliseconds
-			ProcessCommunication();
-			
-			// Drive the LYT bulb
-			LogicLYTLamps(LYTLIGHT1);		
-		} 
+        // Is an unsual approach, but to get fast response to color change we run the LYT logic and
+        // basic communication processing at maximum speed.
+        LogicLYTLamps(LYTLIGHT1);       
+        LogicLYTLamps(LYTLIGHT2);  
+        LogicLYTLamps(LYTLIGHT3);  
+        ProcessCommunication();
+        
+        // Here we process all communication with other nodes
+        FAST_1110ms() {
+            LYTState(LYTLIGHT1);
+            LYTState(LYTLIGHT2);
+            LYTState(LYTLIGHT3);
+        }
 		
-	FAST_510ms() {	
+		FAST_510ms() {	
 	  
-		// If the capacitive button has been press
-		if(digitalRead(8))	{
-			
-			// Toggle between increasing and decreasing
-			if(toggleswitch)	toggleswitch=0;
-			else				toggleswitch=1;
-			
-			// Set the color as white
-			mInput(LYTLIGHT1+1) = 0xF0;
-			mInput(LYTLIGHT1+2) = 0xF0;
-			mInput(LYTLIGHT1+3) = 0xF0;
-			mInput(LYTLIGHT1) = Souliss_T1n_Set;		
-			LogicLYTLamps(LYTLIGHT1);
-			
-			while(digitalRead(8))
-			{
-				// Select if bright up or down
-				if(toggleswitch) mInput(LYTLIGHT1) = Souliss_T1n_BrightUp;
-				else			 mInput(LYTLIGHT1) = Souliss_T1n_BrightDown;
-
-				// Drive the LYT bulb
+			// If the capacitive button has been press
+			if(digitalRead(8))	{
+				
+				// Toggle between increasing and decreasing
+				if(toggleswitch)	toggleswitch=0;
+				else				toggleswitch=1;
+				
+				// Set the color as white
+				mInput(LYTLIGHT1+1) = 0xF0;
+				mInput(LYTLIGHT1+2) = 0xF0;
+				mInput(LYTLIGHT1+3) = 0xF0;
+				mInput(LYTLIGHT1) = Souliss_T1n_Set;		
 				LogicLYTLamps(LYTLIGHT1);
-				delay(250);	
-			}	
-    }	
+				
+				while(digitalRead(8))
+				{
+					// Select if bright up or down
+					if(toggleswitch) mInput(LYTLIGHT1) = Souliss_T1n_BrightUp;
+					else			 mInput(LYTLIGHT1) = Souliss_T1n_BrightDown;
 
-	// Process analogue values
-	FAST_9110ms() {
-		
-		Logic_Light(LIGHTINTESITY);
-		Logic_Humidity(HUMIDITY);
-		Logic_Temperature(TEMPERATURE);
-		
-	}
+					// Drive the LYT bulb
+					LogicLYTLamps(LYTLIGHT1);
+					delay(250);	
+				}	
+			}	
+
+		// Process analogue values
+		FAST_9110ms() {
+			
+			Logic_Light(LIGHTINTESITY);
+			Logic_Humidity(HUMIDITY);
+			Logic_Temperature(TEMPERATURE);
+			
+		}
 	
 }
 		// Here we process all communication with other nodes
-		FAST_GatewayComms();	
+		FAST_PeerComms();	
 	}	
 	
 	EXECUTESLOW() {
 		UPDATESLOW();
 		
 		// Slowly shut down the lamp
-		SLOW_10s() {
-			LYTSleepTimer(LYTLIGHT1);
+		SLOW_x10s(15) {
 			
 			// Read the light intensity
 			AnalogIn(A0, LIGHTINTESITY, 0.1, 0);
@@ -137,5 +150,21 @@ void loop()
 			th = dht.readTemperature();
 			ImportAnalog(TEMPERATURE, &th);			
 		}
+
+        SLOW_x10s(9) {
+            LYTStateRequest(LYTLIGHT1);     // Request the lamp state
+            LYTSleepTimer(LYTLIGHT1);       // Slowly shut down the lamp
+        }
+
+        SLOW_x10s(11) {
+            LYTStateRequest(LYTLIGHT1);     // Request the lamp state
+            LYTSleepTimer(LYTLIGHT1);       // Slowly shut down the lamp
+        }
+
+        SLOW_x10s(13) {
+            LYTStateRequest(LYTLIGHT1);     // Request the lamp state
+            LYTSleepTimer(LYTLIGHT1);       // Slowly shut down the lamp
+        }
+
 	}
 } 
---
+++ examples/plinio/e01_LivingRoom_ESP8266/e01_LivingRoom_ESP8266.ino
@@ -0,0 +1,75 @@
+/**************************************************************************
+    Souliss - LYT Light Bulb
+    
+    This sketch control the Authometion LYT bubls through the Authometion
+    shield.
+    
+    Load this sketch on an ESP8266 WiFi module used to bridge via USART the
+	Arduino AVR microcontroller that handle the radio.
+
+    Verify shield's jumpers and select Hardware Usart while using this sketch, 
+    remember to remove the jumpers before programming the Arduino. Use pin 10 as
+	chip select for the radio.
+        
+***************************************************************************/
+
+// Include framework code and libraries
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <EEPROM.h>
+#include <WiFiUdp.h>
+
+// Configure the framework
+#include "bconf/MCU_ESP8266.h"              // Load the code directly on the ESP8266
+#include "conf/Gateway.h"                   // The main node is the Gateway, we have just one node
+#include "conf/usart_fast.h"                // USART / RS485 transceiver
+#include "conf/DynamicAddressing.h"         // Use dynamically assigned addresses
+#include "conf/WEBCONFinterface.h"          // Enable the WebConfig interface
+
+#include "Souliss.h"
+
+void setup()
+{   
+    Initialize();
+
+    // Read the IP configuration from the EEPROM, if not available start
+    // the node as access point
+    if(!ReadIPConfiguration()) 
+	{	
+		// Start the node as access point with a configuration WebServer
+		SetAccessPoint();
+		startWebServer();
+
+		// We have nothing more than the WebServer for the configuration
+		// to run, once configured the node will quit this.
+		while(1)
+		{
+			yield();
+			runWebServer(); 
+		}
+
+	}
+
+    // Set as Gateway
+    SetAsGateway(myvNet_dhcp);       // Set this node as gateway for SoulissApp  
+
+    // This is the vNet address for this node, used to communicate with other
+    // nodes in your Souliss network
+    SetAddress(0xCE01, 0xFF00, 0x0000);
+    SetAddress(0xAB01, 0xFF00, 0x0000);
+    SetAsPeerNode(0xCE02, 1);
+}
+
+void loop()
+{ 
+    // Here we start to play
+    EXECUTEFAST() {                     
+        UPDATEFAST();   
+             
+        // Process the communication basic at max speed, this allow smooth handling of color and music synch                                
+        ProcessCommunication(); 
+            
+        // Complete the communication tasks at normal rate
+        FAST_GatewayComms();
+    }
+}   
