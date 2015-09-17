bce1f711f33cf5438e4932d6ffd15cf1513b7f48
dario <dario.dimaio@souliss.net>
2015-09-17 22:16:56 +0200

    Updated examples

--- examples/plinio/e01_LivingRoom/e01_LivingRoom.ino
+++ examples/plinio/e01_LivingRoom/e01_LivingRoom.ino
@@ -29,10 +29,13 @@
 #include <SoftwareSerial.h>
 
 // Define logic slots, multicolor lights use four slots
-#define LYTLIGHT1			0	
+#define	LYTLIGHT0			0	
 #define	LIGHTINTESITY		4
 #define	TEMPERATURE			6
 #define	HUMIDITY			8
+
+#define	LowMood									0xFF01,0x01
+#define	FullLight								0xFF01,0x02
 
 uint8_t toggleswitch = 0;
 
@@ -52,13 +55,13 @@ void setup()
     SetAddress(0xCE02, 0xFF00, 0xCE01);
 	
     // Se the LYT bulbs (index, bulb type, addr_a, addr_b, logic slot), here we have
-    // the first lamp (index 0) with default addresses (0, 0). Change these based on
+    // the first lamp (index 0) with default addresses (0, 1). Change these based on
     // the configuration of your lamps.
     InitLYT();
-    SetLYT(0, 0, 0, LYTLIGHT1);
+    SetLYT(0, 0, 1, LYTLIGHT0);
 
     // Define a logic to handle the bulb(s)
-    SetLYTLamps(LYTLIGHT1); 
+    SetLYTLamps(LYTLIGHT0); 
 	Set_Light(LIGHTINTESITY);
 	Set_Temperature(TEMPERATURE);
 	Set_Humidity(HUMIDITY);
@@ -68,18 +71,53 @@ void loop()
 { 
 	// Here we start to play
 	EXECUTEFAST() {						
-		UPDATEFAST();	
-		
-        // Is an unsual approach, but to get fast response to color change we run the LYT logic and
-        // basic communication processing at maximum speed.
-        LogicLYTLamps(LYTLIGHT1);       
+		UPDATEFAST();	       
+
+		LogicLYTLamps(LYTLIGHT0);
         ProcessCommunication();
-        
-        // Here we process all communication with other nodes
-        FAST_1110ms() {
-            LYTState(LYTLIGHT1);
-        }
 		
+		// Listen for topic published by other nodes
+		FAST_110ms() {
+			
+			// Set LowMood
+			if(subscribe(LowMood))
+			{
+				mInput(LYTLIGHT0) = Souliss_T1n_OffCmd;
+				LogicLYTLamps(LYTLIGHT0);
+
+				// Turn on one light
+				LYTOn(LYTLIGHT0);
+				delay(50);
+
+				mInput(LYTLIGHT0+1) = 0x64;
+				mInput(LYTLIGHT0+2) = 0x23;
+				mInput(LYTLIGHT0+3) = 0x00;
+				mInput(LYTLIGHT0) = Souliss_T1n_Set;		
+				LogicLYTLamps(LYTLIGHT0);				
+			}
+
+			// Set Full Light
+			if(subscribe(FullLight))
+			{
+				mInput(LYTLIGHT0) = Souliss_T1n_OnCmd;
+				LogicLYTLamps(LYTLIGHT0);
+				delay(50);
+
+				mInput(LYTLIGHT0+1) = 0xFF;
+				mInput(LYTLIGHT0+2) = 0xFF;
+				mInput(LYTLIGHT0+3) = 0xFF;
+				mInput(LYTLIGHT0) = Souliss_T1n_Set;		
+				LogicLYTLamps(LYTLIGHT0);				
+			}
+
+
+		}
+
+		FAST_1110ms() {
+			// Check for a state update
+			LYTState(LYTLIGHT0);			
+		}
+
 		FAST_510ms() {	
 	  
 			// If the capacitive button has been press
@@ -90,38 +128,36 @@ void loop()
 				else				toggleswitch=1;
 				
 				// Set the color as white
-				mInput(LYTLIGHT1+1) = 0xF0;
-				mInput(LYTLIGHT1+2) = 0xF0;
-				mInput(LYTLIGHT1+3) = 0xF0;
-				mInput(LYTLIGHT1) = Souliss_T1n_Set;		
-				LogicLYTLamps(LYTLIGHT1);
+				mInput(LYTLIGHT0+1) = 0xF0;
+				mInput(LYTLIGHT0+2) = 0xF0;
+				mInput(LYTLIGHT0+3) = 0xF0;
+				mInput(LYTLIGHT0) = Souliss_T1n_Set;		
+				LogicLYTLamps(LYTLIGHT0);
 				
 				while(digitalRead(8))
 				{
 					// Select if bright up or down
-					if(toggleswitch) mInput(LYTLIGHT1) = Souliss_T1n_BrightUp;
-					else			 mInput(LYTLIGHT1) = Souliss_T1n_BrightDown;
+					if(toggleswitch) 
+						mInput(LYTLIGHT0) = Souliss_T1n_BrightUp;
+					else
+						mInput(LYTLIGHT0) = Souliss_T1n_BrightDown;
 
 					// Drive the LYT bulb
-					LogicLYTLamps(LYTLIGHT1);
+					LogicLYTLamps(LYTLIGHT0);
 					delay(250);	
 				}	
 			}	
-
 		}
 
 		// Process analogue values
 		FAST_9110ms() {
-			
 			Logic_Light(LIGHTINTESITY);
 			Logic_Humidity(HUMIDITY);
 			Logic_Temperature(TEMPERATURE);
-			
-		}
-	
 
-		// Here we process all communication with other nodes
-		FAST_PeerComms();	
+			// Request the actual state of the lamp
+			LYTStateRequest();
+		}
 	}	
 	
 	EXECUTESLOW() {
@@ -141,9 +177,7 @@ void loop()
 			th = dht.readTemperature();
 			ImportAnalog(TEMPERATURE, &th);			
 
-            LYTStateRequest(LYTLIGHT1);     // Request the lamp state
-            LYTSleepTimer(LYTLIGHT1);       // Slowly shut down the lamp
+            LYTSleepTimer(LYTLIGHT0);       // Slowly shut down the lamp
         }
-
 	}
 } 
