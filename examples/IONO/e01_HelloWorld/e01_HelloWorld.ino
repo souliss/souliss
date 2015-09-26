7f611826ce329091230669a6c108d4e1feebc7e0
dario <dario.dimaio@souliss.net>
2015-09-26 10:37:55 +0200

    Fix, compiles now

--- examples/IONO/e01_HelloWorld/e01_HelloWorld.ino
+++ examples/IONO/e01_HelloWorld/e01_HelloWorld.ino
@@ -2,11 +2,11 @@
     Souliss - Hello World
     
     Sample sketch to use all the input and outputs of the IONO Ethernet board
-	configure the network IP parameters as per your needs and use SoulissApp
-	to control the board.
+    configure the network IP parameters as per your needs and use SoulissApp
+    to control the board.
 
-	Get started in the wiki
-	https://github.com/souliss/souliss/wiki
+    Get started in the wiki
+    https://github.com/souliss/souliss/wiki
     
 ***************************************************************************/
 
@@ -30,8 +30,8 @@ uint8_t ip_gateway[4]  = {192, 168, 1, 1};
 #define LIGHT4                  3           
 #define LIGHT5                  4   
 #define LIGHT6                  5
-#define PWMLOGIC				6
-#define PWMOUT					7   
+#define PWMLOGIC                6
+#define PWMOUT                  7   
 
 void setup()
 {   
@@ -45,7 +45,7 @@ void setup()
     Set_T11(LIGHT4); 
     Set_T11(LIGHT5); 
     Set_T11(LIGHT6); 
-	Set_T19(PWMLOGIC);   
+    Set_T19(PWMLOGIC);   
     
     // Setup the network configuration
     //
@@ -71,44 +71,44 @@ void loop()
 
         FAST_210ms() {   // Process logics and outputs
  
-            Logic_T11(LIGHT1);                          		// Execute the logic for Relay 1
-            Logic_T11(LIGHT2);                          		// Execute the logic for Relay 2
-            Logic_T11(LIGHT3);                          		// Execute the logic for Relay 3
-            Logic_T11(LIGHT4);                          		// Execute the logic for Relay 4
-            Logic_T11(LIGHT5);                          		// Execute the logic for Relay 5
-            Logic_T11(LIGHT6);                          		// Execute the logic for Relay 6
-			Logic_T19(PWMLOGIC);									// Execute the logic for PWM Output
+            Logic_T11(LIGHT1);                                  // Execute the logic for Relay 1
+            Logic_T11(LIGHT2);                                  // Execute the logic for Relay 2
+            Logic_T11(LIGHT3);                                  // Execute the logic for Relay 3
+            Logic_T11(LIGHT4);                                  // Execute the logic for Relay 4
+            Logic_T11(LIGHT5);                                  // Execute the logic for Relay 5
+            Logic_T11(LIGHT6);                                  // Execute the logic for Relay 6
+            Logic_T19(PWMLOGIC);                                // Execute the logic for PWM Output
             
-            DigOut(RELAY1, Souliss_T1n_Coil, LIGHT1);           // Drive the Relay 1
-            DigOut(RELAY2, Souliss_T1n_Coil, LIGHT2);           // Drive the Relay 2
-            DigOut(RELAY3, Souliss_T1n_Coil, LIGHT3);           // Drive the Relay 3
-            DigOut(RELAY4, Souliss_T1n_Coil, LIGHT4);           // Drive the Relay 4
-            DigOut(RELAY3, Souliss_T1n_Coil, LIGHT5);           // Drive the Relay 5
-            DigOut(RELAY4, Souliss_T1n_Coil, LIGHT6);           // Drive the Relay 6
-			analogWrite(AO1, mOutput(PWMOUT));
+            DigOut(DO1, Souliss_T1n_Coil, LIGHT1);              // Drive the Relay 1
+            DigOut(DO2, Souliss_T1n_Coil, LIGHT2);              // Drive the Relay 2
+            DigOut(DO3, Souliss_T1n_Coil, LIGHT3);              // Drive the Relay 3
+            DigOut(DO4, Souliss_T1n_Coil, LIGHT4);              // Drive the Relay 4
+            DigOut(DO5, Souliss_T1n_Coil, LIGHT5);              // Drive the Relay 5
+            DigOut(DO6, Souliss_T1n_Coil, LIGHT6);              // Drive the Relay 6
+            analogWrite(AO1, mOutput(PWMOUT));
   
         } 
         
         // Here we process all communication with other nodes
-		// and user interface
+        // and user interface
         FAST_GatewayComms();    
     
-		FAST_21110ms() {	// Process the timers in about 2 seconds 
+        FAST_21110ms() {    // Process the timers in about 2 seconds 
 
-			// Timed values are based on number of execution of the timers, as example 
-			// if the timer is set at 20 cycle it will need 40 seconds to exipre.
+            // Timed values are based on number of execution of the timers, as example 
+            // if the timer is set at 20 cycle it will need 40 seconds to exipre.
             Timer_T11(LIGHT1);
             Timer_T11(LIGHT2);  
             Timer_T11(LIGHT3);
             Timer_T11(LIGHT4);
             Timer_T11(LIGHT5);
             Timer_T11(LIGHT6);  
-			Timer_T19(PWMOUT);
-		}
+            Timer_T19(PWMOUT);
+        }
 
-	}
+    }
     
-	// Scheduler for lower execution rates
+    // Scheduler for lower execution rates
     EXECUTESLOW() { 
         UPDATESLOW();
 
