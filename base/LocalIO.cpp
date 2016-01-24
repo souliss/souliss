735920fbb0fb6985e2ccf339b67c4b4b8c5f569c
fazioa <fazioa@gmail.com>
2016-01-24 23:57:24 +0100

    Bugfix Souliss_PulseDigOut and Souliss_PulseLowDigOut

--- base/LocalIO.cpp
+++ base/LocalIO.cpp
@@ -555,6 +555,9 @@ void Souliss_PulseDigOut(U8 pin, U8 value, U8 *memory_map, U8 slot)
 	{
 		OutPin[pin] = PINRESET;
 		dWrite(pin, LOW);
+	} else
+	{	
+		dWrite(pin, LOW);
 	}
 }
 
@@ -594,6 +597,9 @@ void Souliss_PulseLowDigOut(U8 pin, U8 value, U8 *memory_map, U8 slot)
 	else if (memory_map[MaCaco_OUT_s + slot] != value)
 	{
 		OutPin[pin] = PINRESET;
+		dWrite(pin, HIGH);
+	} else
+	{	
 		dWrite(pin, HIGH);
 	}
 }
